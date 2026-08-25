2e13a4764387a6274f3b3c25dce0c80c69f98e79
plainact-crypto <plainact@gmail.com>
2026-08-25 12:36:33 +0300

    Add guide routes and unique dua page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 ﻿import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { AboutPage, ArticlesSourcesPage, DisclaimerPage, HajjLevelsPage, HajjSystemsWorldPage, HowItWorksPage, NotFoundPage, PrivacyPage, RitualsPage } from './pages/ContentPages';
+import { ArticlePage, GuidesPage } from './pages/Articles';
 import { HomePage } from './pages/HomePage';
 import TripsPage from './pages/TripsPage';
 import UmrahProgramCheckerPage from './pages/UmrahProgramCheckerPage';
 
 export default function App() {
-  return <Routes><Route element={<Layout />}><Route index element={<HomePage />} /><Route path="about" element={<AboutPage />} /><Route path="how-it-works" element={<HowItWorksPage />} /><Route path="hajj-levels" element={<HajjLevelsPage />} /><Route path="rituals" element={<RitualsPage />} /><Route path="duas" element={<RitualsPage />} /><Route path="hajj-systems-world" element={<HajjSystemsWorldPage />} /><Route path="articles-sources" element={<ArticlesSourcesPage />} /><Route path="umrah-1448-checker" element={<UmrahProgramCheckerPage />} /><Route path="privacy" element={<PrivacyPage />} /><Route path="disclaimer" element={<DisclaimerPage />} /><Route path="*" element={<NotFoundPage />} /></Route><Route path="/trips" element={<TripsPage />} /></Routes>;
+  return <Routes><Route element={<Layout />}><Route index element={<HomePage />} /><Route path="about" element={<AboutPage />} /><Route path="how-it-works" element={<HowItWorksPage />} /><Route path="hajj-levels" element={<HajjLevelsPage />} /><Route path="rituals" element={<RitualsPage />} /><Route path="hajj-systems-world" element={<HajjSystemsWorldPage />} /><Route path="articles-sources" element={<ArticlesSourcesPage />} /><Route path="guides" element={<GuidesPage />} /><Route path="guides/:slug" element={<ArticlePage />} /><Route path="umrah-1448-checker" element={<UmrahProgramCheckerPage />} /><Route path="privacy" element={<PrivacyPage />} /><Route path="disclaimer" element={<DisclaimerPage />} /><Route path="*" element={<NotFoundPage />} /></Route><Route path="/trips" element={<TripsPage />} /></Routes>;
 }
